10dcb11d5418e176a8f62834fce000db9ef2073a
gvm <79202946014@yandex.ru>
2020-03-15 05:47:00 +0300

    Do index.html

--- venv/app.py
+++ venv/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-from data import title, departures, tours
+from data import title, departures, tours, subtitle, description
 
 app = Flask(__name__)
 
 @app.route('/')
 def render_index_html():
-    return render_template('index.html', title_page=title, nav=departures)
+    tour = []
+    for i in range(1, 7):
+        tour.append(tours[i])
+    return render_template('index.html', title_page=title, nav=departures, sub_title=subtitle, des=description, tour=tour)
 
 @app.route('/departure/<departure>')
 def render_departure(departure):
